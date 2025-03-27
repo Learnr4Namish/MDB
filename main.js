59f5d3a1a89b181d86e5e93bfa6f39ddaf34df56
Learnr4Namish <namishkumar000@gmail.com>
2025-03-27 23:25:31 +0530

    Added a few more commands

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Made by Namish Kumar in March 2025
 // Source code for MDB CLI (Modified Debugging Bridge Command Line Interface)
 console.clear();
-let commands = ["shutdown", "restart", "reconnect", "screenshot", "battery", "battery-level", "stop-charge", "start-charge", "bluetooth"];
+let commands = ["shutdown", "restart", "reconnect", "screenshot", "battery", "battery-level", "stop-charge", "start-charge", "bluetooth", "volup", "voldown", "dial", "show_contacts", "lock", "unlock", "exit"];
 const readline = require('readline');
 const { format } = require('date-fns');
 const exec = require("child_process").exec;
@@ -38,15 +38,17 @@ function reconnect() {
 
 function startShell() {
   rl.question(`$MDB> `, (command) => {
-     if(command === "exit") {
-        console.log("Exiting Wireless MDB CLI...");
-        rl.close();
-        process.exit(0);
-     }else if(command.length === 0) {
+   const arguments = command.split(" ");
+     if(command.length === 0) {
         console.log("MDB Shell:- Please enter a valid command.");
         startShell();
-     }else if(commands.includes(command)) {
-        if(command === "shutdown") {
+     }else if(arguments[0] === "exit") {
+      console.log("Exiting Wireless MDB CLI...");
+      rl.close();
+      process.exit(0);
+     }
+     else if(commands.includes(arguments[0])) {
+        if(arguments[0] === "shutdown") {
            exec(`adb -s ${ip_address_global} shell reboot -p`, (error, stdout, stderr) => {
               if(stderr) {
                  console.error("MDB Shell:- An error occured while trying to shutdown the device:-");
@@ -57,7 +59,7 @@ function startShell() {
               startShell();
               }
            });
-        }else if(command === "restart") {
+        }else if(arguments[0] === "restart") {
               exec(`adb -s ${ip_address_global} shell reboot`, (error, stdout, stderr) => {
                 if(stderr) {
                    console.error("MDB Shell:- An error occured while trying to restart the device:-");
@@ -68,7 +70,7 @@ function startShell() {
                 startShell();  
                 }
             });
-        }else if(command === "reconnect") {
+        }else if(arguments[0] === "reconnect") {
             exec(`adb disconnect ${ip_address_global}`, (error, stdout, stderr) => {
                 if(stderr) {
                    console.error("MDB Shell:- An error occured while trying to disconnect from the device:-");
@@ -79,7 +81,7 @@ function startShell() {
                 reconnect();
                 }
             });
-        }else if(command === "screenshot") {
+        }else if(arguments[0] === "screenshot") {
            exec(`adb -s ${ip_address_global} exec-out screencap -p > screenshot.png`, (error, stdout, stderr) => {
               if(stderr) {
                  console.error("MDB Shell:- An error occured while trying to take a screenshot of the device:-");
@@ -90,7 +92,7 @@ function startShell() {
               startShell();
               }
            });
-        }else if(command === "battery") {
+        }else if(arguments[0] === "battery") {
            exec(`adb -s ${ip_address_global} shell dumpsys battery`, (error, stdout, stderr) => {
               if(stderr) {
                  console.error("MDB Shell:- An error occured while trying to get the battery status of the device:-");
@@ -101,7 +103,7 @@ function startShell() {
               startShell();
               }
            });
-        }else if(command === "battery-level") {
+        }else if(arguments[0] === "battery-level") {
            exec(`adb -s ${ip_address_global} shell dumpsys battery | grep level`, (error, stdout, stderr) => {
               if(stderr) {
                  console.error("MDB Shell:- An error occured while trying to get the battery level of the device:-");
@@ -112,7 +114,7 @@ function startShell() {
               startShell();
               }
            });
-        }else if(command === "stop-charge") {
+        }else if(arguments[0] === "stop-charge") {
          exec(`adb -s ${ip_address_global} shell dumpsys battery set ac 0`, (error, stdout, stderr) => {
             if(stderr) {
                console.error("MDB Shell:- An error occured while trying to stop the device from charging:-");
@@ -123,7 +125,7 @@ function startShell() {
             startShell();
             }
          });
-        }else if(command === "start-charge"){
+        }else if(arguments[0] === "start-charge"){
          exec(`adb -s ${ip_address_global} shell dumpsys battery set ac 1`, (error, stdout, stderr) => {
             if(stderr) {
                console.error("MDB Shell:- An error occured while trying to start the device from charging:-");
@@ -134,7 +136,7 @@ function startShell() {
             startShell();
             }
          });
-        }else if(command === "bluetooth") {
+        }else if(arguments[0] === "bluetooth") {
          exec(`adb -s ${ip_address_global} shell service call bluetooth_manager 6`, (error, stdout, stderr) => {
             if(stderr) {
                console.error("MDB Shell:- An error occured while trying to toggle the bluetooth status of the device:-");
@@ -145,6 +147,88 @@ function startShell() {
             startShell();
             }
          });
+        }else if(arguments[0] === "volup") {
+         exec(`adb -s ${ip_address_global} shell input keyevent 24`, (error, stdout, stderr) => {
+            if(stderr) {
+               console.error("MDB Shell:- An error occured while trying to increase the volume of the device:-");
+               logError(stderr);
+               startShell();
+            }else{
+            console.log("MDB Shell:- Volume of the device has been increased.");
+            startShell();
+            }
+         });
+        }else if(arguments[0] === "voldown"){
+          exec(`adb -s ${ip_address_global} shell input keyevent 25`, (error, stdout, stderr) => {
+             if(stderr) {
+                console.error("MDB Shell:- An error occured while trying to decrease the volume of the device:-");
+                logError(stderr);
+                startShell();
+             }else{
+             console.log("MDB Shell:- Volume of the device has been decreased.");
+             startShell();
+             }
+          });
+        }else if(arguments[0] === "dial"){
+            exec(`adb -s ${ip_address_global} shell am start -a android.intent.action.CALL -d tel:${arguments[1]}`, (error, stdout, stderr) => {
+               if(stderr) {
+                  console.error("MDB Shell:- An error occured while trying to dial the number on the device:-");
+                  logError(stderr);
+                  startShell();
+               }else{
+               console.log("MDB Shell:- Dialing the number on the device.");
+               startShell();
+               }
+            });
+        }else if(arguments[0] === "show_contacts"){
+            exec(`adb -s ${ip_address_global} shell content query --uri content://contacts/phones/`, (error, stdout, stderr) => {
+               if(stderr) {
+                  console.error("MDB Shell:- An error occured while trying to show the contacts on the device:-");
+                  logError(stderr);
+                  startShell();
+               }else{
+               console.log("MDB Shell:- Contacts on the device:-", stdout);
+               startShell();
+               }
+            });
+        }else if(arguments[0] === "lock"){
+         exec(`adb -s ${ip_address_global} shell input keyevent 26`, (error, stdout, stderr) => {
+            if(stderr) {
+               console.error("MDB Shell:- An error occured while trying to lock the device:-");
+               logError(stderr);
+               startShell();
+            }else{
+            console.log("MDB Shell:- Device has been locked.");
+            startShell();
+            }
+         });
+        }else if(arguments[0] === "unlock"){
+            exec(`adb -s ${ip_address_global} shell input keyevent 82`, (error, stdout, stderr) => {
+               if(stderr) {
+                  console.error("MDB Shell:- An error occured while trying to unlock the device:-");
+                  logError(stderr);
+                  startShell();
+               }else{
+                  exec(`adb -s ${ip_address_global} shell input swipe 500 1000 500 500`, (error, stdout, stderr) => {
+                     if(stderr) {
+                        console.error("MDB Shell:- An error occured while trying to unlock the device:-");
+                        logError(stderr);
+                        startShell();
+                     }else{
+                        exec(`adb -s ${ip_address_global} shell input text ${arguments[1]}`, (error, stdout, stderr) => {
+                           if(stderr) {
+                              console.error("MDB Shell:- An error occured while trying to unlock the device:-");
+                              logError(stderr);
+                              startShell();
+                           }else{
+                           console.log("MDB Shell:- Device has been unlocked.");
+                           startShell();
+                           }
+                        });
+                     }
+                  });
+               }
+            });
         }
     }else{
         console.log("MDB Shell:- Invalid command. Please enter a valid command.");
